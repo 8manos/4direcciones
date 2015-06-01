31b9ad83947766c1c726afb5cdb7c5d52afd337e
Anabelle Handdoek <github@huellaspyp.com>
2015-06-01 09:47:41 -0500

    no fecha en lista proyectos

--- wp-content/themes/4d/lista-proyectos.php
+++ wp-content/themes/4d/lista-proyectos.php
@@ -1,11 +1,11 @@
  <!--ORDENAR PROYECTOS ASÍ: AÑO-P-P-AÑO-P-P // POR FAVOR INGRESARLOS DE MAYOR A MENOR :)-->
   <?php while( $proyectos->have_posts() ) : $proyectos->the_post(); ?>
     <?php if( $year != get_the_time('Y') ){ $year = get_the_time('Y') ?>
-      <li class="hexagon fecha" >
+      <!-- <li class="hexagon fecha" >
         <h4><?php echo $year; ?></h4>
         <div class="face1"></div>
         <div class="face2"></div>
-      </li>
+      </li> -->
     <?php } ?>
     <li <?php post_class( 'hexagon' ); ?> data-href="<?php the_permalink(); ?>" style="background-image:url(images/misc/galeria/PROY-1.png);" onClick="urlProyecto(this)" >
       <div class="hexagon_hov">
