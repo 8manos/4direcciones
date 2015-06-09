1a116213b3e2e5bfe5f3ac8331a1b90a7d050976
Anabelle Handdoek <github@huellaspyp.com>
2015-06-09 13:15:49 -0500

    basic 404

---
+++ wp-content/themes/4d/404.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+<div class="internas i_blog">
+	<section class="content">
+	    <div class="container">
+	    	<h3>404 - <?php _e( 'No encontrado', '4dir' ); ?></h3>
+		</div>
+	</section>
+</div>
+<?php get_footer(); ?>
