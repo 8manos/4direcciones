b815858c96eb8fb1e28eb5cca400a89ea4c374a5
Anabelle Handdoek <github@huellaspyp.com>
2015-06-09 13:53:27 -0500

    analytics

--- wp-content/themes/4d/footer.php
+++ wp-content/themes/4d/footer.php
@@ -1,4 +1,14 @@
 <?php wp_footer(); ?>
 <script src="https://cdn.vhx.tv/assets/api.js" data-vhx-site="4direcciones.vhx.tv"></script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-3409925-31', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </body>
 </html>
